c5e93ba9ae50f17269055e3ecc5f2781a5f6e066
ricardov1 <ricardo@cleverppc.com>
2022-01-30 00:28:42 +0100

    add search bar

--- studdyroom/base/views.py
+++ studdyroom/base/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+import queue
+import re
+from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
+from .models import Room, Topic
+from .forms import RoomForm
 # Create your views here.
+
+
 
 
 rooms = [
@@ -11,7 +18,15 @@ rooms = [
 
 # funtion to render the index page
 def home(request):
-    context = {'rooms': rooms}
+    queue = request.GET.get('q', '')
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=queue) |
+        Q(name__icontains=queue) |
+        Q(description__icontains=queue)
+        )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -20,3 +35,34 @@ def room(request, pk):
         if room['id'] == int(pk):
             context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
